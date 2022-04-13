ffeaaa0e865491f9856ab7b0849b7096b3360fdf
yagomarialva <yago.marialva@gmail.com>
2022-04-13 12:18:57 -0400

    Initializing spring project

---
+++ src/test/java/com/yago/classschedule/ClassScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yago.classschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
